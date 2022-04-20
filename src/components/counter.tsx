d6f03863457053b39d54079342affac6d177afd8
Azhar Uddin Sheikh <azhars@ideafoundation.in>
2022-04-20 12:58:10 +0530

    rendering list, conditioanl rendering

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -3,22 +3,33 @@ import React, { Component } from 'react';
  
 class Counter extends Component {
     state = {
-        count:4,
-        imgUrl : 'https://picsum.photos/200'
+        count:0,
+        tags : []
     };
 
     styles = {
         height: 100
     };
 
-    render() { 
+    renderTags() : JSX.Element {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}> {tag} </li>)}</ul>;
+    }
+
+    render() : JSX.Element { 
+
         return (
         <React.Fragment>
-            <img style={this.styles} src={this.state.imgUrl} alt="" /> <br />
-            <span className='badge badge-primary m-2'>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
+            {this.state.tags.length === 0 && 'Please Create Tags'}
+            {this.renderTags()}
         </React.Fragment>
         );
+    }
+
+    getbadgeClasses() : string {
+        let classes : string = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
     }
 
     formatCount() : string | number{
